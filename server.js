4c69648a314450c78605b469b4bbacacf92bd760
DeMarcus Kirby <tkd024@gmail.com>
2019-08-16 09:41:21 -0600

    Set up initial end point

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 
-// app.set('port', process.env.PORT )
+app.use(express.json());
+app.use(express.static('public'))
+app.set('port', process.env.PORT || 3000 )
+
+app.locals.title = 'Sim Youtube';
+
+app.get('/', (request, response) => {
+  response.send('Inital response setup is working')
+})
+
+app.listen(app.get('port'), () => (
+  console.log(` ${app.locals.title} is running on http://localhost:${app.get('port')}.`)
+))
