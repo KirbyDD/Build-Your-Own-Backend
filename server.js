c812bf8c614015ee69eb7c8280bbf93ee99b807f
DeMarcus Kirby <tkd024@gmail.com>
2019-08-19 08:35:50 -0600

    Add get endpoints

--- server.js
+++ server.js
@@ -20,6 +20,32 @@ app.get('/api/v1/youtubers', (request, response) => {
   .catch(error => response.json(`There was an error gathering the Youtubers`))
 })
 
+app.get('api/v1/videos', (request, response) => {
+  database('youtube_videos').select('*')
+  .then(videos => response.status(200).json(videos))
+  .catch(error => response.json('There was an error gathering the videos')) 
+})
+
+app.get('api/v1/youtubers/:id', (request, response) => {
+  database('youtubers').select('*')
+  .then(youtubers => youtubers.find(youtuber => {
+    let { id } = match.params;
+    return youtuber.id == id
+  }))
+  .then(youtuber => response.status(200).json(youtuber))
+  .catch(error => response.json('There was an error gathering the videos'))
+})
+
+app.get('api/v1/videos/:id', (request, response) => {
+  database('youtube_videos').select('*')
+  .then(videos => videos.find(video => {
+    let { id } = match.params;
+    return video.id == id
+  }))
+  .then(video => response.status(200).json(video))
+  .catch(error => response.json('There was an error gathering the videos'))
+})
+
 app.listen(app.get('port'), () => (
   console.log(` ${app.locals.title} is running on http://localhost:${app.get('port')}.`)
 ))
